59e96466a9b9801991ce20b85bb857c26c00c470
autcouriere <authoritycourier@gmail.com>
2026-07-23 11:45:07 -0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,17 @@
 import mysql.connector
+from urllib.parse import urlparse
+import os
+
+DATABASE_URL = "mysql://root:"wOWSnFrgtrUwNBhJjuIZxHEWbMhCxlYH@sakura.proxy.rlwy.net:59102/railway"
+url = urlparse(DATABASE_URL)
+
+connection = mysql.connector.connect(
+    host=url.hostname,
+    user=url.username,
+    password=url.password,
+    database=url.path.lstrip("/"),
+    port=url.port
+)
 
 def get_connection():
-connection = mysql.connector.connect(
-host="localhost",
-     
-
-    user="root",
-
-        password="",
-
-        database="trustnova_bank"
-
-    )
-
     return connection

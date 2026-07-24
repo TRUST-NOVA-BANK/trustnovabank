ea2d39a6945a9f63a4e378bc600c68f22ef48c5e
autcouriere <authoritycourier@gmail.com>
2026-07-24 00:45:46 -0400

    Create admin.auth.js

---
+++ admin.auth.js
@@ -0,0 +1,35 @@
+document
+.getElementById("adminLoginForm")
+.addEventListener("submit", async function(e){
+
+e.preventDefault();
+
+const email = document.getElementById("admin_email").value;
+const password = document.getElementById("admin_password").value;
+
+
+// Check admin table
+const { data: admin, error } = await supabase
+.from("admins")
+.select("*")
+.eq("email", email)
+.eq("password_hash", password)
+.single();
+
+
+if(error){
+
+    alert("Invalid admin login");
+
+} else {
+
+    alert("Admin login successful!");
+
+    window.location.href = "admin-dashboard.html";
+
+}
+
+});
+
+
+

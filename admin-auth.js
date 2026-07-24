b4bcbb6bd9b058e3b042231e143ec24ca1f4c52f
autcouriere <authoritycourier@gmail.com>
2026-07-24 04:46:30 -0400

    Update admin-auth.js

--- admin-auth.js
+++ admin-auth.js
@@ -1,32 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
+alert("Admin auth connected");
 
-    alert("Admin auth connected");
+document
+.getElementById("adminLoginForm")
+.addEventListener("submit", async function(e){
 
-    const form = document.getElementById("adminLoginForm");
+    e.preventDefault();
 
-    form.addEventListener("submit", async function (e) {
+    const email = document.getElementById("admin_email").value;
+    const password = document.getElementById("admin_password").value;
 
-        e.preventDefault();
 
-        const email = document.getElementById("admin_email").value;
-        const password = document.getElementById("admin_password").value;
+    const { data: admin, error } = await supabase
+        .from("admins")
+        .select("*")
+        .eq("email", email)
+        .eq("password_hash", password)
+        .single();
 
-        const { data: admin, error } = await supabase
-            .from("admins")
-            .select("*")
-            .eq("email", email)
-            .eq("password_hash", password)
-            .single();
 
-        if (error || !admin) {
-            alert("Invalid email or password");
-            return;
-        }
+    if(error || !admin){
 
-        alert("Login successful!");
+        alert("Invalid admin login");
+
+        console.log(error);
+
+    } else {
+
+        alert("Admin login successful!");
+
+        localStorage.setItem(
+            "admin",
+            JSON.stringify(admin)
+        );
 
         window.location.href = "admin-dashboard.html";
 
-    });
+    }
 
 });

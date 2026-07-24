da2d7c97a19db6b2bb539f7b49026ce20f876ac0
autcouriere <authoritycourier@gmail.com>
2026-07-23 23:08:03 -0400

    Initialize Supabase client with URL and key

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "sb_publishable_FUPiaEMQmlO0X7CtZlZU-Q_PjCC3mGD";
+const supabaseKey = "sb_secret_etGbBw6P1riV3SoOScJVmA_Aot5ACVG";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
